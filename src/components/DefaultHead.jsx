84c3c44dd44ac7f159a15d6932851ac2f8e8dcea
최재민 <admin@chicken-moo.com>
2023-06-04 11:33:28 +0900

    first commit

---
+++ src/components/DefaultHead.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+const DefaultHead = ({children}) => {
+  return (
+    <Head>
+      <title></title>
+      <meta name="description" content="" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:image" content="https://bucket.2w.vc/public/vote.png" />
+      <link rel="icon" href="/favicon.ico" />
+      {/* <script defer data-domain="dimigo.net" src="https://analytics.2w.vc/js/script.js"></script> */}
+      {children}
+    </Head>
+  );
+};
+
+export default DefaultHead;
